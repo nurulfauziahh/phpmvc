9afcbf1789a09b8e311939b7e3486e293c6b9992
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-10-07 14:42:52 +0700

    menambahkan assets dan menambahkan konten untuk about

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Halaman About</title>
-</head>
-<body>
-
-	<h1>About Me</h1>
-
-</body>
-</html>
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/pic1.jpg" alt="Nurul Fauziah" width ="200" class="rounded-circle shadow">
+	<p>hallo Nama Saya <?= $data['nama']; ?>, Umur saya  <?= $data['umur']; ?> tahun. Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
