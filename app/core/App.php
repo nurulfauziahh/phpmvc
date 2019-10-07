ae5bb512967c979b79c0f3111f68020febfd8257
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-10-07 13:00:40 +0700

    Menambah file About.php dan Home.php serta menambah controller

--- app/core/App.php
+++ app/core/App.php
@@ -30,4 +30,24 @@ class App {
 		require_once '../app/controllers/'. $this->controller .'.php';
 		$this->controller = new $this->controller;
 
+		//method
+		if( isset($url[1])) {
+			if(method_exists($this->controller, $url[1])){
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+		if(!empty($url)) {
+			$this->params = array_values($url);
+			
+		}
+
+
+		//jalankan controllers dan method serta kirimkan params jika ada
+		call_user_func_array([$this->controller, $this->method], $this->params);
+
+
+	}
+
 }
