1266efda1ff15760e815da30907e749f3b8e79b9
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-10-07 15:33:36 +0700

    menambahkan mahasiswa ke database dengan memakai form

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -14,3 +14,56 @@
 				
 		</div>
 	</div>	
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+	  
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+		<input type="hidden" name="id" id="id">
+		<div class="form-group">
+			<label for="nama">Nama</label>
+			<input type="text" class="form-control" id="nama" name="nama">
+ 		 </div>
+
+		  <div class="form-group">
+			<label for="nrp">nrp</label>
+			<input type="number" class="form-control" id="nrp" name="nrp">
+ 		 </div>
+
+		  <div class="form-group">
+			<label for="email">email</label>
+			<input type="email" class="form-control" id="email" name="email">
+ 		 </div>
+
+		  <div class="form-group">
+			<label for="jurusan">jurusan</label>
+			<select class="form-control" id="jurusan" name="jurusan">
+			<option value="Teknik Informatika">Teknik Informatika</option>
+			<option value="Teknik Industri">Teknik Industri</option>
+			<option value="Teknik Pangan">Teknik Pangan</option>
+			<option value="Teknik Planologi">Teknik Planologi</option>
+			<option value="Teknik Lingkungan">Teknik Lingkungan</option>
+			<option value="Teknik Mesin">Teknik Mesin</option>
+
+
+			</select>
+		</div>
+		  
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+		</form>
+      </div>
+    </div>
+  </div>
+</div>
