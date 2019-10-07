1b7aa5c0e8d9ef22ef1eb69ccceede76ba6b5219
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2019-10-07 12:26:22 +0700

    Melakukan Routing di file App.php

--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,33 @@
 <?php 
 
 class App {
-    public function __construct() {
+    public function parseURL() {
 
-
+		if(isset($_GET['url'])) {
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			return $url;
+		}
     }
+    
+
+    protected $controller = 'Home';
+	protected $method = 'index';
+	protected $params = [];
+
+
+
+	public function __construct() {
+
+		$url = $this->parseURL();
+		
+		if(file_exists('../app/controllers/'. $url[0]. '.php')) {
+			$this->controller =  $url[0];
+			unset($url[0]);
+		}
+
+		require_once '../app/controllers/'. $this->controller .'.php';
+		$this->controller = new $this->controller;
 
 }
